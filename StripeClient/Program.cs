a1de3ff4d14795d87da17da0f67d2a34d6c11edf
Aravind <aravindgnair@outlook.com>
2024-06-10 17:51:42 +0530

    Added stripe client

---
+++ StripeClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using Stripe;
+
+namespace StripeClient;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        try
+        {
+            StripeConfiguration.ApiKey = "";
+            const long amount = 100;
+
+            var options = new ChargeCreateOptions
+            {
+                Amount = amount,
+                Currency = "USD",
+                Source = "tok_visa",
+                Description = "Test payment of $1"
+            };
+
+            var service = new ChargeService();
+            var charge = await service.CreateAsync(options);
+
+            Console.WriteLine(charge.StripeResponse.Content);
+        }
+        catch (StripeException e)
+        {
+            Console.WriteLine(e);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+    }
+}
