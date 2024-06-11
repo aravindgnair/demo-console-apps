5a2427cc11cde02f85755a845b2eabf33a478c9e
Aravind <aravindgnair@outlook.com>
2024-06-11 09:16:48 +0530

    StripeAPIClient - added check for customer before creating

--- StripeClient/Program.cs
+++ StripeClient/Program.cs
@@ -1,15 +1,18 @@
 ﻿using Stripe;
 
-namespace StripeClient;
+namespace StripeApiClient;
 
 internal class Program
 {
+    private const string ApiKey = "sk_test_51PFHFuSEKxiNGr7ukue9H13dzIuN1Sl8kmBcqdJ39kk7utau5FmnFJtiJMSWf64kyafqKJ0XYPpcpX8D67KoPKcR00DU1YCy9C";
+    private const string CustomerEmail = "alice@tailspin.com";
+
     static async Task Main(string[] args)
     {
         try
         {
-            StripeConfiguration.ApiKey = "";
-            await CreateCustomerAsync("alice@tailspin.com");
+            StripeConfiguration.ApiKey = ApiKey;
+            await CreateCustomerAsync(CustomerEmail);
             //await MakePaymentAsync();
         }
         catch (StripeException e)
@@ -42,14 +45,35 @@ internal class Program
 
     private static async Task CreateCustomerAsync(string email)
     {
+        
+        if (await IsCustomerExists(email))
+        {
+            throw new Exception("Customer already exists");
+        }
+
+        await CreateCustomer(email);
+    }
+
+    private static async Task<bool> IsCustomerExists(string email)
+    {
+        var service = new CustomerService();
+        await foreach (var customer in service.ListAutoPagingAsync())
+        {
+            if (customer.Email == email)
+                return true;
+        }
+
+        return false;
+    }
+
+    private static async Task CreateCustomer(string email)
+    {
         var options = new CustomerCreateOptions
         {
             Email = email
         };
-
         var service = new CustomerService();
         var customer = await service.CreateAsync(options);
-
-        Console.WriteLine(customer.Email);
+        Console.WriteLine("Customer created successfully");
     }
 }
